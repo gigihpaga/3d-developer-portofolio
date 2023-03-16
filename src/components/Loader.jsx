9b8a432deae3892f848c8a9342de71371a97cc58
gigihpaga <gigihpaga@gmail.com>
2023-03-16 20:40:22 +0700

    first create component Loader for 3D Model 🚀

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Html, useProgress } from '@react-three/drei';
 
 const Loader = () => {
-    return <div>Loader</div>;
+    const { progress } = useProgress();
+    return (
+        <Html>
+            <span className="canvas-loader" />
+            <p style={{ fontSize: 14, color: '#f1f1f1', fontWeight: 400, marginTop: 40 }}>
+                {progress.toFixed(2)}%
+            </p>
+        </Html>
+    );
 };
 
 export default Loader;
