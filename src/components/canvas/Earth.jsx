3d7ee03b92083e45b5a59a18f24460288d4b03dc
gigihpaga <gigihpaga@gmail.com>
2023-03-16 20:53:13 +0700

    initial Earth 3D Model 🚀

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+/* eslint-disable react/no-unknown-property */
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
-const Earth = () => {
-    return <div>Earth</div>;
+import CanvasLoader from '../Loader';
+
+const EarthModel = () => {
+    const earth = useGLTF('./planet/scene.gltf');
+    return <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />;
 };
 
-export default Earth;
+const EarthCanvas = () => {
+    return (
+        <Canvas
+            className="hover:cursor-grab active:cursor-grabbing"
+            // style={{ backgroundColor: '#f58df714' }}
+            frameloop="demand"
+            shadows
+            gl={{ preserveDrawingBuffer: true }}
+            camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
+        >
+            <Suspense fallback={<CanvasLoader />}>
+                <OrbitControls
+                    autoRotate
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                />
+                <EarthModel />
+            </Suspense>
+            <Preload all />
+        </Canvas>
+    );
+};
+
+export default EarthCanvas;
