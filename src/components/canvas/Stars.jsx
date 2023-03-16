66cae18e83fc125a7c508e12832fa5e102bdc5a6
gigihpaga <gigihpaga@gmail.com>
2023-03-16 20:57:36 +0700

    initial Stars 3D Model 🚀

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+/* eslint-disable react/no-unknown-property */
+import React, { useRef, Suspense } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
+import * as MathRandom from 'maath/random/dist/maath-random.esm';
 
-const Stars = () => {
-    return <div>Stars</div>;
+const StarsModel = (props) => {
+    const startModelRef = useRef();
+    const sphere = MathRandom.inSphere(new Float32Array(5000), { radius: 1.2 });
+    useFrame((state, delta) => {
+        startModelRef.current.rotation.x -= delta / 10;
+        startModelRef.current.rotation.y -= delta / 50;
+    });
+
+    return (
+        <group rotation={[0, 0, Math.PI / 4]}>
+            <Points ref={startModelRef} positions={sphere} stride={3} frustumCulled {...props}>
+                <PointMaterial
+                    transparent
+                    color="#f272c8"
+                    size={0.002}
+                    sizeAttenuation={true}
+                    deptWrite={false}
+                />
+            </Points>
+        </group>
+    );
 };
 
-export default Stars;
+const StarsCanvas = () => {
+    return (
+        <div id="StarsCanvas" className="w-full h-auto absolute inset-0 z-[-1]">
+            <Canvas camera={{ position: [0, 0, 1] }}>
+                <Suspense fallback={null}>
+                    <StarsModel />
+                </Suspense>
+                <Preload all />
+            </Canvas>
+        </div>
+    );
+};
+
+export default StarsCanvas;
