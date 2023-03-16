795267f4b68a01f4714aeac693253ddaf1388a6b
gigihpaga <gigihpaga@gmail.com>
2023-03-16 20:30:33 +0700

    first create component App 🖐️

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import {
+    About,
+    Contact,
+    Experience,
+    Feedbacks,
+    Hero,
+    Navbar,
+    StarsCanvas,
+    Tech,
+    Works,
+} from './components';
 const App = () => {
-    return <div className="App">3D Developper</div>;
+    return (
+        <BrowserRouter>
+            <div id="app-wrapper" className="relative z-0 bg-primary">
+                <div
+                    id="navbar-wrapper"
+                    className="bg-hero-pattern bg-cover bg-no-repeat bg-center"
+                >
+                    <Navbar />
+                    <Hero />
+                </div>
+                <About />
+                <Experience />
+                <Tech />
+                <Works />
+                <Feedbacks />
+                <div id="contact-wrapper" className="relative z-0">
+                    <Contact />
+                    <StarsCanvas />
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 };
 
 export default App;
